bf94ab85d1e4e3b078eb1842f78ce9951727a7c1
Sahoo <Rajendra.Kumar.Sahoo@dnv.com>
2023-04-17 17:45:11 +0530

    header and sidebar changes added

--- ecomp/src/app/layout/header/header.component.ts
+++ ecomp/src/app/layout/header/header.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  hideNav: any;
+  currentUser: any;
+  loggedInUserName = '';
+  loggedInUserShortName = '';
+  @HostListener('document:click', ['$event']) onDocumentClick(event: any) {
+    this.hideNav = false;
+  }
+  constructor(
+    public router: Router
+  ) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.hideNav = false; // when navigated close the side nav
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.currentUser = 'User Name';
+    this.loggedInUserName = 'User Name';
+    this.loggedInUserShortName = 'UN';
+  }
+
+  onCollapse(event?: any) {
+    this.hideNav = !this.hideNav;
+  }
 }
