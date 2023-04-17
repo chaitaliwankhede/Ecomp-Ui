ba49ab6d78dfb4485b8cbfcd2e01082245b33981
Sahoo <Rajendra.Kumar.Sahoo@dnv.com>
2023-04-17 21:43:29 +0530

    advanced search accordion changes added

--- ecomp/src/app/app.module.ts
+++ ecomp/src/app/app.module.ts
@@ -7,6 +7,15 @@ import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderSidebarComponent } from './layout/header-sidebar/header-sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { AdvancedSearchComponent } from './layout/advanced-search/advanced-search.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, NG_VALIDATORS, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -14,12 +23,22 @@ import { HeaderSidebarComponent } from './layout/header-sidebar/header-sidebar.c
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    HeaderSidebarComponent
+    HeaderSidebarComponent,
+    AdvancedSearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AgGridModule
+    AgGridModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
